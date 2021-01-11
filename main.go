d0b5442187ac5f290a3605e44dbcaa870ebcb707
Brett Wright <brett.wright@bestseller.com>
2021-01-11 13:52:19 +0100

    Undoing last comming

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/BESTSELLER/go-vault/gcpss"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	vaultAddr := os.Getenv("VAULT_ADDR")
+	if vaultAddr == "" {
+		log.Fatal("VAULT_ADDR must be set.")
+	}
+	vaultSecret := os.Getenv("VAULT_SECRET")
+	if vaultSecret == "" {
+		log.Fatal("VAULT_SECRET must be set.")
+	}
+	vaultRole := os.Getenv("VAULT_ROLE")
+	if vaultRole == "" {
+		log.Fatal("VAULT_ROLE must be set.")
+	}
+
+	secret, err := gcpss.FetchVaultSecret(vaultAddr, vaultSecret, vaultRole)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(secret)
+	data := []byte(secret)
+	err = ioutil.WriteFile("/secrets/secrets", data, 0644)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
